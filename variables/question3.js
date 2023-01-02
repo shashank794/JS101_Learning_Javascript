16ea89fc9f1212be82bfb5d41d3912127e4fe7d9
Shashank Shekhar <shekhars794@gmail.com>
2023-01-02 21:04:31 +0530

    First commit

---
+++ variables/question3.js
@@ -0,0 +1,12 @@
+let a="꧁༒༻☬ད  ཌ☬༺༒꧂";
+console.log(a+"\n\n")
+let name = "꧁𓊈𒆜🆂 🅷 🅰 🆂 🅷 🅰 🅽 🅺𒆜𓊉꧂";
+    console.log(name +"\n");
+console.log(typeof ( "\n"+name ));
+
+let age =23;
+console.log("\n\n"+age);
+
+console.log(typeof( age ));
+b="꧁༒༻☬ད  ཌ☬༺༒꧂";
+console.log("\n\n"+b);
